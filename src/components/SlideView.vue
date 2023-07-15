69f756dec29f6d99170ca7787c43dc1918cacce1
aaron <alkrauss48@gmail.com>
2023-07-14 21:24:51 -0500

    feat: implement textfit

--- src/components/SlideView.vue
+++ src/components/SlideView.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-});
+import { ref, onMounted } from 'vue'
+import textfit from 'textfit';
+
+const slideContent = ref(null)
+
+const runTextFit = () => {
+  textfit(slideContent.value, { maxFontSize: 500 });
+}
+
+onMounted(runTextFit);
+
+addEventListener("resize", runTextFit)
 </script>
 
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div id="slide">
+    <div ref="slideContent" style="width:90%;height:90%">
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+    </div>
   </div>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#slide {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
