62efc9f30be92b2f45e0e3e2949e0d75145dc578
aaron <alkrauss48@gmail.com>
2023-07-16 22:20:35 -0500

    feat: install and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
