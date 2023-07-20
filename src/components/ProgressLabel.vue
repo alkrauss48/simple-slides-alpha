86c1bb6763e6f8ba5397ac1f4614355a59bafb36
aaron <alkrauss48@gmail.com>
2023-07-19 23:32:51 -0500

    style: update progress label to be bottom centered

--- src/components/ProgressLabel.vue
+++ src/components/ProgressLabel.vue
@@ -9,7 +9,7 @@ defineProps<{
 <template>
   <aside class="
     [ text-white bg-gray-400 ]
-    [ fixed bottom-0 right-0 ]
+    [ fixed bottom-0 left-1/2 -ml-12 ]
     [ h-8 w-24 ]
     [ flex justify-center items-center ]
   ">
